b99b4bcc2234073278a6e99ec03083945983b4f0
andreapaci <61983905+andreapaci@users.noreply.github.com>
2020-03-28 19:03:33 +0100

    Commit numero 1: Project setup files

---
+++ src/logic/Main.java
@@ -0,0 +1,15 @@
+package logic;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+	
+	public static void main(String[] args) {
+	
+		LOGGER.info("Logger name: " + LOGGER.getName());
+		
+	}
+
+}
